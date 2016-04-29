b58bccf1e1194c40e2e15fa66a7f9e2e29b72595
Stefan <stefan-koopman@hotmail.com>
2016-04-28 22:12:33 -1000

    CORE - added softcore impl

---
+++ project/Nios.v
@@ -0,0 +1,13 @@
+module Nios (
+clk,
+rst_n,
+led,
+);
+input clk, rst_n;
+output [7:0] led;
+DE2_115_QSYS DE2_115_QSYS_inst (
+.clk_clk(clk),
+.reset_reset_n(rst_n),
+.led_export(led),
+);
+endmodule
---
+++ project/software/Hello_Nios/obj/default/hello_world.d
@@ -0,0 +1,14 @@
+obj/default/hello_world.o: hello_world.c ../Hello_Nios_bsp/system.h \
+ ../Hello_Nios_bsp/linker.h \
+ ../Hello_Nios_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../Hello_Nios_bsp//HAL/inc/io.h ../Hello_Nios_bsp//HAL/inc/alt_types.h
+
+../Hello_Nios_bsp/system.h:
+
+../Hello_Nios_bsp/linker.h:
+
+../Hello_Nios_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../Hello_Nios_bsp//HAL/inc/io.h:
+
+../Hello_Nios_bsp//HAL/inc/alt_types.h:
